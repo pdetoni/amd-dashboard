7749e373130a50dbf040c947b6b515086ed26881
pdetoni <pdpereira2003@gmail.com>
2024-06-12 15:12:37 -0300

    duas tabelas ok

--- models/dbmngr.js
+++ models/dbmngr.js
@@ -1,3 +1,13 @@
-const sqlite = require('better-sqlite3') 
-const db = new sqlite("./roip.db")
-exports.db = db
+// const sqlite = require('better-sqlite3') 
+// const db = new sqlite("./roip.db")
+// exports.db = db
+
+const Database = require('better-sqlite3');
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, './roip.db');
+const db = new Database(dbPath, { verbose: console.log });
+
+module.exports = {
+    db
+};
